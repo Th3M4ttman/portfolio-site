525a2d07ae5595b709b2ef52156e96341fc6cbac
Th3M4ttman <WorkMatthewJHarris94@gmail.com>
2021-09-29 20:59:28 +0100

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const createProxyMiddleware = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/API',
     createProxyMiddleware({
-      target: 'https://matt-harris-portfolio.herokuapp.com:5000/API/',
+      target: 'http://localhost:5000/',
       changeOrigin: true,
     })
   );
